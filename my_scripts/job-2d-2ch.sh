81418eb42988b2fa355fc539e8973672d61e746b
Fei Zhou <zhou6@llnl.gov>
2021-10-19 11:58:11 -0700

    scripts

--- my_scripts/job-2d-2ch.sh
+++ my_scripts/job-2d-2ch.sh
@@ -7,26 +7,26 @@
 
 # conda activate powerai
 PYTHON=~/lassen-space/conda-ibm/envs/powerai/bin/python
-DAT=$HOME/data/grain/square_justNN_noduplicate
+DAT=$HOME/data/grain/2D-2Ch
 
 device=0
-for nlayer in 7 9; do 
-for nlmlp in 2 3; do 
+
+for nlayer in 11 13; do 
 for buf in 1; do
-    for N1 in 2; do
+    for N1 in 1; do
         for lr in 3e-4; do # 1e-2 3e-3 1e-3; do
-            for nfeat in 64; do #150 180 80 128
+            for nfeat in 128 ; do #150 180 80 128
 for symm in cubic ; do #""
-for act in mish; do #mish swish relu tanh
-for threshold in 2e-3; do 
-            DIR=meshgraphnets/experiment/grain-nfeat${nfeat}_nlayer${nlayer}_symm${symm}_act${act}_threshold${threshold}_nlmlp${nlmlp}
+for act in relu swish; do #swish
+for threshold in 1e-3; do 
+            DIR=meshgraphnets/experiment/grain2ch-nfeat${nfeat}_nlayer${nlayer}_symm${symm}_act${act}_threshold${threshold}
             mkdir -p $DIR
             CUDA_VISIBLE_DEVICES=$device TF_CPP_MIN_LOG_LEVEL=2 $PYTHON -m meshgraphnets.run_model \
  --dataset_dir=$DAT --checkpoint_dir=$DIR \
- --model=NPS --unique_op=false --periodic=1 --nfeat_in=1 --nfeat_out=1 --nfeat_latent=$nfeat --n_mpassing=$nlayer \
+ --model=NPS --unique_op=false --periodic=1 --nfeat_in=2 --nfeat_out=2 --nfeat_latent=$nfeat --n_mpassing=$nlayer \
  --amr_N=64 --amr_N1=$N1 --amr_buffer=$buf --amr_threshold=$threshold --amr_eval_freq=5 \
  --mode=train \
- --num_training_steps=500000 --batch=8 --lr=$lr --lr_decay=70000 --cache --keep_ckpt=1 --valid_freq=8000 --rotate=$symm --mlp_activation=$act \
+ --num_training_steps=500000 --batch=8 --lr=$lr --lr_decay=70000 --cache --keep_ckpt=2 --valid_freq=8000 --rotate=$symm --mlp_activation=$act \
  --noise=0.02 --rollout_split=test --num_rollouts=100 &>>$DIR/log &
             device=$((device+1))
 done
@@ -36,7 +36,6 @@ done
             done
         done
     done
-done
 done
 wait
 
